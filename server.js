f4dae78f63445c689829a69b234922d05618cab0
Your Name <your-email@example.com>
2025-06-04 13:05:09 +0530

    Your message describing the changes

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
-import bodyParser from 'express'
-import userRouter from './Routes/user.js'
-
-
-
+import userRouter from './Routes/user.js';
 
 const app = express();
- app.use(bodyParser.json())
 
- 
-//home testing
-app.get('/',(req,res)=>res.json({message:'this is a home routes'}))
+// Middleware
+app.use(express.json());
 
+// Home Route
+app.get('/', (req, res) => res.json({ message: 'this is a home route' }));
 
+// User Routes
+app.use('/api/user', userRouter);
 
-//user Router
-app.use('/api/user',userRouter)
-
-
-
+// MongoDB Connection
 mongoose.connect(
-  "mongodb+srv://rajurajsekhar123:OFD0uhvoCWfiyUSx@cluster0.gjyqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
-    dbName:"MERN_E_commerce"
+  "mongodb+srv://rajurajsekhar123:OFD0uhvoCWfiyUSx@cluster0.gjyqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    dbName: "MERN_E_commerce"
   }
-).then(()=>console.log("Mongodb Connected Successfully..")).catch((err)=>console.log(err));
+)
+.then(() => console.log("MongoDB Connected Successfully.."))
+.catch((err) => console.error("MongoDB connection error:", err));
 
-
-
-const port= 1000;
-app.listen(port,()=>console.log(`Server is running on port ${port}`))
+// Server
+const port = 1000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
